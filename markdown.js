a3d3653b4e1d0382da27ad8063020799bbeed6fe
Dario Cangialosi a.k.a. Arkenidar <dario.cangialosi@gmail.com>
2025-10-19 15:17:38 +0200

    [fix] improve showdown script loading and setup process

--- markdown.js
+++ markdown.js
@@ -1,16 +1,24 @@
 var showdownScript = document.createElement('script')
+
+// When the script is loaded, set up the markdown viewers
+showdownScript.onload = setupMarkdownViewers
+
 ///showdownScript.src = 'https://cdn.jsdelivr.net/npm/showdown@2.1.0/dist/showdown.min.js'
 ///showdownScript.src = 'https://arkenidar.com/app/lib/html-markdown/showdown.min.js'
 showdownScript.src = 'https://arkenidar.github.io/html-markdown/showdown.min.js'
 
-var scriptNode
-scriptNode = document.currentScript
-scriptNode.insertAdjacentElement('afterend', showdownScript)
-
-addEventListener('load', setupMarkdownViewers)
+// Insert the script after the current script tag
+document.currentScript.insertAdjacentElement('afterend', showdownScript)
 
 function setupMarkdownViewers() {
 
+    // need to wait until showdown is loaded
+    if (typeof showdown === 'undefined') {
+        setTimeout(setupMarkdownViewers, 50)
+        return
+    }
+
+    // Now we can use showdown
     var showdownConverter = new showdown.Converter()
 
     function viewContent(content, node) {
